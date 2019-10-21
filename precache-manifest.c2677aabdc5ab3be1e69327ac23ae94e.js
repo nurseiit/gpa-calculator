2618e1ddd21189201228b69cc9f47b052df4098a
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2019-10-22 04:32:25 +0900

    Updates

---
+++ precache-manifest.c2677aabdc5ab3be1e69327ac23ae94e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b3f3f4d3f8e9c5119c8bc8b0ef840bf",
+    "url": "/gpa-calculator/index.html"
+  },
+  {
+    "revision": "7f59801dee490b2c2010",
+    "url": "/gpa-calculator/static/css/2.aa7fcd32.chunk.css"
+  },
+  {
+    "revision": "9b8a898ebc4086c9f17f",
+    "url": "/gpa-calculator/static/css/main.ef59e689.chunk.css"
+  },
+  {
+    "revision": "7f59801dee490b2c2010",
+    "url": "/gpa-calculator/static/js/2.a7984fb3.chunk.js"
+  },
+  {
+    "revision": "9b8a898ebc4086c9f17f",
+    "url": "/gpa-calculator/static/js/main.3f7a37ad.chunk.js"
+  },
+  {
+    "revision": "358c72e961f289af9fa9",
+    "url": "/gpa-calculator/static/js/runtime-main.248a6c68.js"
+  }
+]);
